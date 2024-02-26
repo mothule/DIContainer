71d195e31572730fdc224db7197ebd0f9f6d895f
mothule <mothule.dev+github@gmail.com>
2024-02-26 21:51:42 +0900

    Change scopes

--- Sources/DIContainer/DIContainer.swift
+++ Sources/DIContainer/DIContainer.swift
@@ -1,18 +1,18 @@
 public class DIContainer {
-    static var shared: DIContainer = .init()
+    public static var shared: DIContainer = .init()
     
-    typealias Factory = (DIContainer) -> Any
+    public typealias Factory = (DIContainer) -> Any
     
     private var instances: [String: Factory] = [:]
     
     @discardableResult
-    func register<T>(_ type: T.Type, factory: @escaping Factory) -> Self {
+    public func register<T>(_ type: T.Type, factory: @escaping Factory) -> Self {
         let key = String(describing: type)
         instances[key] = factory
         return self
     }
     
-    func resolve<T>() -> T {
+    public func resolve<T>() -> T {
         let key = String(describing: T.self)
         
         guard let factory = instances[key] else {
